b8a02150391eab1d40c422502309d455a7a6a126
jhoysbou <neeroll21@gmail.com>
2020-05-26 23:33:54 +0300

    tempdir

--- src/test/java/ru/mail/polis/CommitLogTest.java
+++ src/test/java/ru/mail/polis/CommitLogTest.java
@@ -1,6 +1,7 @@
 package ru.mail.polis;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 import ru.mail.polis.jhoysbou.commitlog.CommitLog;
 import ru.mail.polis.jhoysbou.commitlog.LogRecord;
 
@@ -13,8 +14,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CommitLogTest extends BasicTest{
 
     @Test
-    void test() throws IOException {
-        CommitLog commitLog = new CommitLog(new File("/Users/jhoysbou/Documents/git_repositories/2020-db-lsm"));
+    void test(@TempDir File data) throws IOException {
+        CommitLog commitLog = new CommitLog(data);
         LogRecord logRecord = new LogRecord(randomKey(), randomValue(), (byte) 0);
         commitLog.log(randomKey(), randomValue(), (byte) 0);
         Iterator<LogRecord> iterator = commitLog.provideData();
